f16dcf897a11274f099f7ab008bfca2966900c72
5ranke <wjdtnwls0106@gmail.com>
2026-08-04 14:21:38 +0900

    Init Spring Boot project

---
+++ src/main/java/com/sujin/todayjang/TodayJangApplication.java
@@ -0,0 +1,13 @@
+package com.sujin.todayjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayJangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayJangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sujin/todayjang/TodayJangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujin.todayjang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayJangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
